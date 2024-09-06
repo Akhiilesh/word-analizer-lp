7381799a88696584df7acb82d6b8ab24a7ce085e
Akhilesh Sanap <akhileshsanap091@gmail.com>
2024-09-06 13:33:08 +0530

    Components make reusable in Stats

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,22 +1,19 @@
 export default function Stats() {
   return (
     <section className="stats">
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Words</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Characters</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">280</span>
-        <h2 className="second-heading">Instagram</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">2200</span>
-        <h2 className="second-heading">Facebook</h2>
-      </section>
+      <Stat lable={"Words"} num={"0"} />
+      <Stat lable={"Characters"} num={"0"} />
+      <Stat lable={"Instagram"} num={"280"} />
+      <Stat lable={"FaceBook"} num={"2200"} />
+    </section>
+  );
+}
+
+function Stat({ lable, num }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{num}</span>
+      <h2 className="second-heading">{lable}</h2>
     </section>
   );
 }
