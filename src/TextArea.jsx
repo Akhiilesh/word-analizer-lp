92cf986a785f59008ac8bfd16dc2b2c0eb272176
Akhilesh Sanap <akhileshsanap091@gmail.com>
2024-09-06 13:06:02 +0530

    Unwanted useState remove

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -3,18 +3,15 @@ import Warning from "./Warning";
 
 function TextArea() {
   const [text, setText] = useState("");
-  const [warning, setWarning] = useState(false);
   const [warningText, setWarningText] = useState("");
 
   const handleChange = (e) => {
     let newText = e.target.value;
     if (newText.includes("<script>")) {
       setWarningText("No <script> Tag Allowed!");
-      setWarning(true);
       newText = newText.replace("<script>", "");
     } else if (newText.includes("@")) {
       setWarningText("No @ Symbol Allowed!");
-      setWarning(true);
       newText = newText.replace("@", "");
     }
     setText(newText);
@@ -28,7 +25,7 @@ function TextArea() {
         placeholder="Enter your text..."
         spellCheck="false"
       />
-      {warning ? <Warning warningText={warningText} /> : null}
+      {warningText ? <Warning warningText={warningText} /> : null}
     </div>
   );
 }
