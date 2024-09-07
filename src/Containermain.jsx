a5cd69aab87abeb898c4ed978a7ab4355196a015
Akhilesh Sanap <akhileshsanap091@gmail.com>
2024-09-07 16:00:11 +0530

    Calculation and variable declaration put together in an object rightway

--- src/Containermain.jsx
+++ src/Containermain.jsx
@@ -4,16 +4,12 @@ import TextArea from "./TextArea";
 
 export default function Containermain() {
   const [text, setText] = useState("");
-  const numofChar = text.length;
-  const instaCharLeft = 280 - text.length;
-  const facebookCharLeft = 2200 - text.length;
-  const countWords = text.split(/\s/).filter((word) => word !== "").length;
 
   const stats = {
-    numofChar: numofChar,
-    instaCharLeft: instaCharLeft,
-    facebookCharLeft: facebookCharLeft,
-    countWords: countWords
+    numofChar: text.length,
+    instaCharLeft: 280 - text.length,
+    facebookCharLeft: 2200 - text.length,
+    countWords: text.split(/\s/).filter((word) => word !== "").length
   };
 
   return (
